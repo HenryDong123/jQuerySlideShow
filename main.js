befeec17f416d7e2cf7676f72a8e5a13831089f3
HenryDong123 <943082205@qq.com>
2018-10-19 18:34:22 +0800

    新无缝轮播 基础轮播功能实现

--- main.js
+++ main.js
@@ -1,42 +1,14 @@
-let n
-Initialization()
-setInterval(()=>{
-    makeLeave(getImage(n))
-        .one('transitionend', (e)=>{
-            makeEnter($(e.currentTarget))
-        })
-    makeCurrent(getImage(n+1))
-    n += 1
-},2500)
+let $buttons = $('#buttonWrapper>button')
+let $slides = $('#slides')
 
+$buttons.eq(0).on('click',function () {
+    $slides.css({transform:'translateX(0px)'})
+})
 
+$buttons.eq(1).on('click', function () {
+    $slides.css({transform: 'translateX(-450px)'})
+})
 
-function getImage(n){
-    return $(`.images > img:nth-child(${x(n)})`)
-}
-
-function x(n){
-    if(n>3){
-        n = n%3
-        if (n===0){
-            n =3
-        }
-    } // n = 1 2 3
-    return n
-}
-
-function Initialization(){
-    n = 1
-    $(`.images > img:nth-child(${n})`).addClass('current')
-        .siblings().addClass('enter')
-}
-
-function makeCurrent($node){
-    return $node.removeClass('enter leave').addClass('current')
-}
-function makeLeave($node){
-    return $node.removeClass('enter current').addClass('leave')
-}
-function makeEnter($node){
-    return $node.removeClass('leave current').addClass('enter')
-}
+$buttons.eq(2).on('click', function () {
+    $slides.css({transform: 'translateX(-900px)'})
+})
